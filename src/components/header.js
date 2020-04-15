031810299675fc5ed84dd08f836d2dd0679e8b3b
CharlieDFX <61200179+CharlieDFX@users.noreply.github.com>
2020-04-15 11:19:43 -0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ import { Auth } from 'aws-amplify'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'gold',
+      background: 'mediumseagreen',
       marginBottom: '1.45rem',
     }}
   >
